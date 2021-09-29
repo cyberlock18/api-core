843d12a9d11ddcf2754450b21e9cd3756846d5e7
cyberlock18 <ruben.oizquierdo@alumnos.upm.es>
2021-09-29 14:58:30 +0200

    MI PRIMER COMMI (ADD MENSAGE)T

--- routers/router.js
+++ routers/router.js
@@ -3,6 +3,6 @@ router.get('/', function (req, res) {
     res.json({
         status: 'API ESTA FUNCIONANDO',
         message: 'Welcome to RESTHub crafted with love!'
-    });
+    }); //modification
 });
 module.exports = router;
