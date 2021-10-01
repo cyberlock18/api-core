daa7eabbebc128ecf90aa88e17bfcd559e378bfd
cyberlock18 <ruben.oizquierdo@alumnos.upm.es>
2021-10-01 20:23:40 +0200

    intento de continuacion

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const mainrouter = require('./routers/router');
-const app = express();
-const port = process.env.PORT || 8080;
-app.use('/api',mainrouter);
-app.listen(port, function () {
-  console.log("Running RestHub on port " + port);
-});
--- routers/router.js
+++
@@ -1,8 +0,0 @@
-let router = require('express').Router();
-router.get('/', function (req, res) {
-    res.json({
-        status: 'API ESTA FUNCIONANDO',
-        message: 'Welcome to RESTHub crafted with love!'
-    }); //modification
-});
-module.exports = router;
